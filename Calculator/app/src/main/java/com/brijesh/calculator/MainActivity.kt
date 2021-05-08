b7920c0adcf1b829146ff231c7983abd362dab33
Brijesh <brijeshivam@gmail.com>
2021-05-08 12:42:11 +0530

    RealTime result updated and minor bugs fixed

--- Calculator/app/src/main/java/com/brijesh/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/brijesh/calculator/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.brijesh.calculator
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import net.objecthunter.exp4j.*
+import org.w3c.dom.Text
+import java.lang.Integer.parseInt
+import java.math.BigDecimal
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,15 +18,35 @@ class MainActivity : AppCompatActivity() {
     var inputCopy ="a"
     var dotUsed: Boolean = false
     var result: Double = 0.0
-
+    var resultString = "0"
+    var r : BigDecimal? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val te:TextView = findViewById(R.id.tv2)
+        val tr:TextView = findViewById(R.id.tv3)
+        te.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if(input == "-"||input ==""||isLastOp()||input ==tr.text){
+                    return
+                }
+                resultOfExpression(te)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
+
+
 
 
     }
      fun write(v: View){
-        when (v.getId()){
+        when (v.id){
             R.id.btn1 -> {
                 if (input.length > 46) {
                     return
@@ -113,7 +138,7 @@ class MainActivity : AppCompatActivity() {
             R.id.btnc -> {
                 input = ""
                 findViewById<TextView>(R.id.tv2).text = ""
-                findViewById<TextView>(R.id.tv3).text = ""
+                findViewById<TextView>(R.id.tv3).text = "0"
                 dotUsed = false
             }
 
@@ -178,6 +203,10 @@ class MainActivity : AppCompatActivity() {
                 if (input.length > 46 || input == "") {
                     return
                 }
+                if(input =="-"){
+                    input = input.dropLast(1)
+                    findViewById<TextView>(R.id.tv2).text = input
+                    return}
                 if (input.length!=1 &&input[input.lastIndex] =='-'&&(input[input.lastIndex-1] =='*'|| input[input.lastIndex-1] =='/')) {
                     input = input.dropLast(1)
                     findViewById<TextView>(R.id.tv2).text = input
@@ -207,7 +236,7 @@ class MainActivity : AppCompatActivity() {
                 if (input.length > 46 || input == "") {
                     return
                 }
-                if(input.equals("-"))
+                if(input == "-")
                 {
                     input = input.dropLast(1)
                     findViewById<TextView>(R.id.tv2).text = input
@@ -231,10 +260,10 @@ class MainActivity : AppCompatActivity() {
                 }
                 if (input.endsWith("+") || input.endsWith("*") || input.endsWith("-") || input.endsWith("/")) {
                     for (i in input.lastIndex - 1 downTo 0) {
-                        if (input.get(i) == '+' || input.get(i) == '-' || input.get(i) == '*' || input.get(i) == '/') {
+                        if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/') {
                             dotUsed = false
                         }
-                        if (input.get(i) == '.') {
+                        if (input[i] == '.') {
                             dotUsed = true
                         }
 
@@ -249,16 +278,15 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-    fun isLastOp():Boolean{
+    private fun isLastOp():Boolean{
         return input.endsWith(".")||input.endsWith("+")||input.endsWith("*")||input.endsWith("-")||input.endsWith("/")
     }
 
     fun resultOfExpression(v: View) {
         if(input == inputCopy){
-            inputCopy = result.toString().dropLast(2)
+            inputCopy =  resultString
             findViewById<TextView>(R.id.tv2).text = inputCopy
-            findViewById<TextView>(R.id.tv3).text = ""
-            input = inputCopy
+            input = resultString
             return
         }
 
@@ -269,10 +297,21 @@ class MainActivity : AppCompatActivity() {
         try {
              result = ExpressionBuilder(input)
                     .build()
-                    .evaluate()
+                     .evaluate()
 
-            findViewById<TextView>(R.id.tv3).text = result.toString().dropLast(2)
+            r = BigDecimal(result.toString())
+
+            resultString  = if(r!!.toPlainString() .endsWith(".0")){
+                r!!.toPlainString().dropLast(2)
+            }else if(r!!.toPlainString().length>16 && r!!.toPlainString().contains(".")){
+                r!!.toPlainString().subSequence(0,16) as String
+            }
+            else{
+                r!!.toPlainString()
+            }
+            findViewById<TextView>(R.id.tv3).text = resultString
             inputCopy = input
+
         } catch ( e: Exception ) {
             Toast.makeText(this, "Enter Correct Expression", Toast.LENGTH_SHORT)
                     .show()
