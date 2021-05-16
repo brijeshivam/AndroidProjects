d082f901d76be21aff7a07c8c8e060b08fd46c80
Brijesh <brijeshivam@gmail.com>
2021-05-16 19:33:49 +0530

    Working app

--- RelaxBro/app/src/main/java/com/brijesh/relaxbro/Home.kt
+++ RelaxBro/app/src/main/java/com/brijesh/relaxbro/Home.kt
@@ -32,7 +32,7 @@ class Home : AppCompatActivity() {
     var title: String? = ""
     var i =0;
     override fun onCreate(savedInstanceState: Bundle?) {
-        //sleep(3000)
+
         super.onCreate(savedInstanceState)
 
         setTheme(R.style.Theme_RelaxBro)
